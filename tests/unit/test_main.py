aaa6e87cd444607fd17d75493ca8451699a1246e
sti <sti@matera.com>
2022-10-14 16:09:30 -0300

    Atualização Teste de Unidade usando uma lista como massa de teste

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import somar, dividir
 
 
@@ -10,6 +12,7 @@ def teste_somar():
 
     assert resultado_obtido == resultado_esperado
 
+
 def teste_dividir_positivo():
     numero_a = 27
     numero_b = 3
@@ -18,6 +21,7 @@ def teste_dividir_positivo():
     resultado_obtido = dividir(numero_a, numero_b)
 
     assert resultado_obtido == resultado_esperado
+
 
 def teste_dividir_negativo():
     numero_a = 27
@@ -29,3 +33,20 @@ def teste_dividir_negativo():
     assert resultado_obtido == resultado_esperado
 
 
+# lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+
+]
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+
+
+    resultado_obtido = somar(numero_a, numero_b)
+
+    assert resultado_obtido == resultado_esperado
